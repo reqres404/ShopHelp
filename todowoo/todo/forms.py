94e0609aa9610d1d6babfd94e5143520e591732d
Aditya Patil <adittyapatil89@gmail.com>
2023-02-01 13:44:13 +0530

    version 1

--- todowoo/todo/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm, fields
-from .models import Todo
-
-class TodoForm(ModelForm):
-    class Meta:
-        model = Todo
-        fields =['title','memo','important']
